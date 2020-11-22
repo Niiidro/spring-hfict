12667fa687cfbdeb42001bc7f254f71ca3b14ae8
Philipp Minder <philipp.minder@hotmail.com>
2020-11-22 20:31:17 +0100

    Initial commit

---
+++ src/main/java/ch/minder/springhfict/SpringBootAuftragApplication.java
@@ -0,0 +1,13 @@
+package ch.minder.springhfict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuftragApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAuftragApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/minder/springhfict/SpringBootAuftragApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.minder.springhfict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuftragApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
