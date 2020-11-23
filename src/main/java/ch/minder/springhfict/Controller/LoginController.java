d34d8a56d9c43dd3a028e52fdb7fd4ffb955f002
Philipp Minder <philipp.minder@hotmail.com>
2020-11-23 16:40:16 +0100

    Finished PostMapping

--- src/main/java/ch/minder/springhfict/Controller/LoginController.java
+++ src/main/java/ch/minder/springhfict/Controller/LoginController.java
@@ -4,17 +4,22 @@ import ch.minder.springhfict.model.Login;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 
 
 @RestController
 public class LoginController {
 
     @PostMapping("/login")
-    public Login login(@RequestBody Login login) {
+    public Login login(@RequestBody Login login, HttpServletResponse response) {
+        if (login.getUsername().equals("test") && login.getPassword().equals("1234")) {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
         return login;
+
     }
 }
