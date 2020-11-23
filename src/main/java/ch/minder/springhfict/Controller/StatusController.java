70eb14326552aecb9709078a6c73fbf9976c610f
Philipp Minder <philipp.minder@hotmail.com>
2020-11-23 12:29:19 +0100

    Stuck in Post returning HTTP Status

---
+++ src/main/java/ch/minder/springhfict/Controller/StatusController.java
@@ -0,0 +1,15 @@
+package ch.minder.springhfict.Controller;
+
+import ch.minder.springhfict.model.Status;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public Status status(@RequestParam(value = "status", defaultValue = "Ok") String status) {
+        return new Status(status);
+    }
+}
